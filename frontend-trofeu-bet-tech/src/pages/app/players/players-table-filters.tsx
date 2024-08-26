39b0a306856833eed9f43cacc87e98713b005f47
Carlos Eduardo <74209989+caduce3@users.noreply.github.com>
2024-08-26 10:52:52 -0300

    feat: componentizando filtro de players

--- frontend-trofeu-bet-tech/src/pages/app/players/players-table-filters.tsx
+++ frontend-trofeu-bet-tech/src/pages/app/players/players-table-filters.tsx
@@ -1,11 +1,26 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Search, X } from "lucide-react";
 
 const PlayersTableFilters = () => {
     return ( 
         <form className="flex items-center gap-2">
             <span className="text-sm font-semibold">Filtros:</span>
-            <Input placeholder="ID do jogador" className="h-8 w-[320px]" />
-            <Input placeholder="Nome do jogador" className="h-8 w-[320px]" />
+            <Input placeholder="ID do jogador" className="h-8 w-auto" />
+            <Input placeholder="Nome do jogador" className="h-8 w- w-auto" />
+            <Input placeholder="Telefone do jogador" className="h-8 w-auto" />
+            <Input placeholder="E-mail do jogador" className="h-8 w- w-auto" />
+            <Input placeholder="CPF do jogador" className="h-8 w-auto" />
+
+            <Button type="submit" variant="secondary" size="sm">
+                <Search className="mr-2 h-4 w-4"/>
+                Filtrar resultados
+            </Button>
+
+            <Button type="button" variant="outline" size="sm">
+                <X className="mr-2 h-4 w-4"/>
+                Remover filtros
+            </Button>
         </form>
      );
 }
