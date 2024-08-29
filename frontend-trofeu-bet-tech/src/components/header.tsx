5a685e6ae069d535d2ab9f33f6621b889464a2bb
Carlos Eduardo <74209989+caduce3@users.noreply.github.com>
2024-08-29 13:10:23 -0300

    feat: adicionando logo e icon

--- frontend-trofeu-bet-tech/src/components/header.tsx
+++ frontend-trofeu-bet-tech/src/components/header.tsx
@@ -3,12 +3,16 @@ import { Dices, Home, Users } from "lucide-react";
 import { NavLink } from "./nav-link";
 import { ThemeToggle } from "./theme/theme-toggle";
 import AccountMenu from "./account-menu";
+import logo from "../assets/logoTrofeu.svg"
 
 const Header = () => {
     return ( 
         <div className="border-b">
             <div className="flex h-16 items-center gap-6 px-6">
                 {/* <div>LOGO</div> */}
+                <div>
+                    <img src={logo} alt="Trofeu.bet" className="h-6"/>
+                </div>
                 <Separator orientation="vertical" className="h-6"/>
 
                 <nav className="flex items-center space-x-4 lg:space-x-6">
