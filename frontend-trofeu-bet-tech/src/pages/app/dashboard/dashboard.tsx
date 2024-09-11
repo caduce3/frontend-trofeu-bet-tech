136ee704c195a5aa4e21d72c4c7b21dbbdfef8c5
Carlos Eduardo <74209989+caduce3@users.noreply.github.com>
2024-09-11 16:02:45 -0300

    feat: subindo teste de tempo para renderização de gráfico

--- frontend-trofeu-bet-tech/src/pages/app/dashboard/dashboard.tsx
+++ frontend-trofeu-bet-tech/src/pages/app/dashboard/dashboard.tsx
@@ -1,7 +1,7 @@
 import { Helmet } from "react-helmet-async";
-import {  GraficoLtv } from "./grafico-ltv-jogadores";
+// import {  GraficoLtv } from "./grafico-ltv-jogadores";
 import { useAuthRedirect } from "@/middlewares/authRedirect";
-import { GraficoLtvDepositos } from "./grafico-ltv-depositos";
+// import { GraficoLtvDepositos } from "./grafico-ltv-depositos";
 import { GraficoTicketMedio } from "./grafico-ticket-medio";
 
 
@@ -19,12 +19,12 @@ export function Dashboard(){
             <div className="flex flex-col gap-4">
                 <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
 
-                <div className="gap-4">
+                {/* <div className="gap-4">
                     <GraficoLtv />
                 </div>
                 <div className="gap-4">
                     <GraficoLtvDepositos />
-                </div>
+                </div> */}
                 <div className="gap-4">
                     <GraficoTicketMedio />
                 </div>
