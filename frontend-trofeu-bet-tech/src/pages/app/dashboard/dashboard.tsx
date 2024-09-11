acd7a6b9842d37c6d90033ec68370436f1936d20
Carlos Eduardo <74209989+caduce3@users.noreply.github.com>
2024-09-11 16:10:19 -0300

    fix: correção e upando gráficos

--- frontend-trofeu-bet-tech/src/pages/app/dashboard/dashboard.tsx
+++ frontend-trofeu-bet-tech/src/pages/app/dashboard/dashboard.tsx
@@ -1,8 +1,8 @@
 import { Helmet } from "react-helmet-async";
-// import {  GraficoLtv } from "./grafico-ltv-jogadores";
+import {  GraficoLtv } from "./grafico-ltv-jogadores";
 import { useAuthRedirect } from "@/middlewares/authRedirect";
-// import { GraficoLtvDepositos } from "./grafico-ltv-depositos";
-import { GraficoTicketMedio } from "./grafico-ticket-medio";
+import { GraficoLtvDepositos } from "./grafico-ltv-depositos";
+// import { GraficoTicketMedio } from "./grafico-ticket-medio";
 
 
 export function Dashboard(){
@@ -19,15 +19,15 @@ export function Dashboard(){
             <div className="flex flex-col gap-4">
                 <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
 
-                {/* <div className="gap-4">
+                <div className="gap-4">
                     <GraficoLtv />
                 </div>
                 <div className="gap-4">
                     <GraficoLtvDepositos />
-                </div> */}
-                <div className="gap-4">
-                    <GraficoTicketMedio />
                 </div>
+                {/* <div className="gap-4">
+                    <GraficoTicketMedio />
+                </div> */}
 
             </div>
         </>
