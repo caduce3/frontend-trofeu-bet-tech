51b8e978c319c8d5353a849dd0a42b41474a6a1a
Carlos Eduardo <74209989+caduce3@users.noreply.github.com>
2024-09-23 13:40:20 -0300

    feat: Componentizando filtros da seção de tráfego

---
+++ frontend-trofeu-bet-tech/src/pages/app/trafego/trafego-table-filters.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Filter } from "lucide-react";
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import * as React from "react";
+import { DatePickerWithRange } from "@/components/date-ranger-picker";
+
+interface TrafegoTableFiltersProps {
+    dateRange: { from: Date; to: Date };
+    setDateRange: React.Dispatch<React.SetStateAction<{ from: Date; to: Date }>>;
+}
+
+const TrafegoTableFilters: React.FC<TrafegoTableFiltersProps> = ({ dateRange, setDateRange }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <Dialog open={isOpen} onOpenChange={setIsOpen}>
+            <DialogTrigger asChild>
+                <Button variant="outline" size="sm" className="flex items-center bg-[#18181B] rounded-lg" onClick={() => setIsOpen(true)}>
+                    <span className="text-sm font-semibold mr-2">Filtros</span>
+                    <Filter className="h-4 w-4" />
+                </Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Filtros</DialogTitle>
+                </DialogHeader>
+                <div className="pt-1 pb-1 pr-0.5">
+                    <DatePickerWithRange
+                        className="w-full"
+                        value={{ from: dateRange.from, to: dateRange.to }}
+                        onChange={(range) => {
+                            if (range?.from && range?.to) {
+                                setDateRange({ from: range.from, to: range.to });
+                            }
+                        }}
+                    />
+                </div>
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+export default TrafegoTableFilters;
--- frontend-trofeu-bet-tech/src/pages/app/trafego/trafego.tsx
+++ frontend-trofeu-bet-tech/src/pages/app/trafego/trafego.tsx
@@ -16,10 +16,12 @@ import { TrafegoTableSkeleton } from "./trafego-table-skeleton";
 import { verifyAccessByJwt } from "@/services/verify-access-page-by-jwt";
 import { useEffect } from "react";
 import { toast } from "sonner";
+import { format } from "date-fns";
+import * as React from "react";
+import TrafegoTableFilters from "./trafego-table-filters";
 
 export function RelatorioTrafego() {
     const token = useAuthRedirect();
-
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -29,52 +31,59 @@ export function RelatorioTrafego() {
         }
     }, [token, navigate]); 
 
+    const [dateRange, setDateRange] = React.useState<{ from: Date; to: Date }>({
+        from: new Date(2024, 6, 1),
+        to: new Date(2024, 6, 31),
+    });
+    
+    const dataInicial = format(dateRange.from, "dd-MM-yyyy");
+    const dataFinal = format(dateRange.to, "dd-MM-yyyy");
+
     const [searchParams, setSearchParams] = useSearchParams();
     const page = searchParams.get('page') ?? 1;
 
     const { data, isLoading } = useQuery({
-        queryKey: ['relatorio-ftds', page],
-        queryFn: () => getRelatorioFtds({page: Number(page), dataInicial: '01-09-2024 10:54', dataFinal: '10-09-2024 10:58'}),
+        queryKey: ['relatorio-ftds', page, dataInicial, dataFinal],
+        queryFn: () => getRelatorioFtds({ page: Number(page), dataInicial, dataFinal }),
     });
 
     function handlePaginate(page: number) {
         setSearchParams(prev => {
             prev.set('page', (page).toString());
-
-            return prev
-        })
+            return prev;
+        });
     }
- 
+
     return (
         <>
-            <Helmet title="Tráfego"/>
+            <Helmet title="Tráfego" />
             <div className="flex flex-row gap-4 items-center">
                 <h1 className="text-3xl font-bold tracking-tight">Relatório FTD</h1>
+                <TrafegoTableFilters dateRange={dateRange} setDateRange={setDateRange} /> {/* Passando props */}
             </div>
             
-            {
-                isLoading ? <TrafegoTableSkeleton />
-                :
-                <Table className=" bg-[#18181B] rounded-xl">
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead>ID Registro</TableHead>
-                            <TableHead className="">UTM Campaign</TableHead>
-                            <TableHead className="hidden md:table-cell">Registros</TableHead>
-                            <TableHead className="hidden sm:table-cell">FTDs</TableHead>
-                            <TableHead className="hidden md:table-cell">Valor FTD</TableHead>
-                            <TableHead className="hidden sm:table-cell">UTM Content</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {data && data.relatorioFtds?.map(relatorio => {
-                            return <RelatorioTrafegoFtdsTableRow key={relatorio.id_registro} relatorioFTDs={relatorio} />
-                        })}
-                    </TableBody>
-                </Table>
+            {isLoading ? <TrafegoTableSkeleton />
+            :
+            <Table className="bg-[#18181B] rounded-xl">
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>ID Registro</TableHead>
+                        <TableHead>UTM Campaign</TableHead>
+                        <TableHead className="hidden md:table-cell">Registros</TableHead>
+                        <TableHead className="hidden sm:table-cell">FTDs</TableHead>
+                        <TableHead className="hidden md:table-cell">Valor FTD</TableHead>
+                        <TableHead className="hidden sm:table-cell">UTM Content</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {data && data.relatorioFtds?.map(relatorio => {
+                        return <RelatorioTrafegoFtdsTableRow key={relatorio.id_registro} relatorioFTDs={relatorio} />;
+                    })}
+                </TableBody>
+            </Table>
             }
             {data && (
-                <Pagination currentPage={data.currentPage} totalPages={data.totalPages} totalItens={data.totalItens} onPageChange={handlePaginate}/>
+                <Pagination currentPage={data.currentPage} totalPages={data.totalPages} totalItens={data.totalItens} onPageChange={handlePaginate} />
             )}
         </>
     )
