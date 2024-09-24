0446a7e498868198ff271e6697faf7fe0787ec35
Carlos Eduardo <74209989+caduce3@users.noreply.github.com>
2024-09-24 15:26:25 -0300

    fix: ajustando lógica de paginação
    Ao chegar na penúltima página, o botão ficava desabilitado para ir para a última página.

--- frontend-trofeu-bet-tech/src/components/pagination.tsx
+++ frontend-trofeu-bet-tech/src/components/pagination.tsx
@@ -31,15 +31,16 @@ export function Pagination({ currentPage, totalPages, totalItens, onPageChange }
                         <ChevronLeft className="h-4 w-4"/>
                         <span className="sr-only">Página anterior</span>
                     </Button>
-                    <Button variant="outline" className="h-8 w-8 p-0" onClick={() => onPageChange(currentPage + 1)} disabled={totalPages <= currentPage + 1}>
+                    <Button variant="outline" className="h-8 w-8 p-0" onClick={() => onPageChange(currentPage + 1)} disabled={totalPages <= currentPage}>
                         <ChevronRight className="h-4 w-4"/>
                         <span className="sr-only">Próxima página</span>
                     </Button>
-                    <Button variant="outline" className="h-8 w-8 p-0" onClick={() => onPageChange(totalPages)} disabled={totalPages <= currentPage + 1}>
+                    <Button variant="outline" className="h-8 w-8 p-0" onClick={() => onPageChange(totalPages)} disabled={currentPage === totalPages}>
                         <ChevronsRight className="h-4 w-4"/>
                         <span className="sr-only">Última página</span>
                     </Button>
                 </div>
+
             </div>
         </div>
     )
